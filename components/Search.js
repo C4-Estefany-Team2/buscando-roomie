e75548bb14c86b6105a3ccd6d27c232bfa29b477
JuanDev <rexejej@gmail.com>
2020-10-03 20:17:22 -0500

    corrrection card and search section

--- components/Search.js
+++ components/Search.js
@@ -6,7 +6,7 @@ import { useFilter } from '../hooks/useFilter'
 // import { useRouter } from 'next/router'
 import { getRoomsByCity } from '../selectors/getRoomsByCity'
 
-const cities = ['', 'Campeche', 'CDMX', 'Chiapas', 'Cuernavaca', 'Puebla']
+const cities = ['C', 'Campeche', 'CDMX', 'Chiapas', 'Cuernavaca', 'Puebla']
 
 const Search = () => {
   // const router = useRouter()
@@ -20,19 +20,23 @@ const Search = () => {
 
   return (
     <>
-      <form className={styles.search}>
+      <div className={styles.container}>
         <label>Escoge una ciudad:</label>
-        <select className={styles.options} id="cities" name="searchText" value={searchText} onChange={handleInputChange}>
-          {cities.map((item, index) => (
-            <option key={index}>{ item }</option>
-          ))}
-        </select>
-      </form>
-      {
-        roomsFiltered.map((item) => (
-          <Card key={item.id} {...item} />
-        ))
-      }
+        <form className={styles.search}>
+          <select className={styles.options} id="cities" name="searchText" value={searchText} onChange={handleInputChange}>
+            {cities.map((item, index) => (
+              <option key={index}>{ item }</option>
+            ))}
+          </select>
+        </form>
+      </div>
+      <div className={styles.containerCard}>
+        {
+          roomsFiltered.map((item) => (
+            <Card key={item.id} {...item} />
+          ))
+        }
+      </div>
     </>
   )
 }
